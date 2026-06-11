61a4e97908620c8b19f6bcb98d4d0a05ad188850
sanikashingan <sanikashingan328@gmail.com>
2026-06-11 10:19:43 +0530

    Create numbers.py

---
+++ python/numbers.py
@@ -0,0 +1,6 @@
+numbers = [12, 45, 7, 89, 34, 56, 23, 90, 11, 67]
+
+print("Numbers:", numbers)
+print("Highest:", max(numbers))
+print("Lowest:", min(numbers))
+print("Average:", sum(numbers) / len(numbers))
